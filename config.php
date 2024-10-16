528ebec03288cbefabd636fffb4f7455cfa08ded
Muhammad Umair Tariq <umairtariq903@yahoo.com>
2024-10-16 03:41:56 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+if (preg_match("/config\.php/", $_SERVER['SCRIPT_NAME']))
+{
+	header('Location:index.php?');
+	exit;
+}
+
+$SITE_TIMEZONE = "Europe/Amsterdam";
+//$SITE_KLASORU = 'D:/xampp/htdocs/PmsServer/';
+$SITE_KLASORU = '/var/www/vhosts/product.360-pms.com/httpdocs/';
+$SITE_URL = "https://".$_SERVER['HTTP_HOST']."/";
+//$SITE_URL = "http://".$_SERVER['HTTP_HOST']."/PmsServer/";
+
+$STATUS = 'PRODUCTION' ; // TEST | PRODUCTION
+require_once 'knjiz/LibLoader.php';
+//require_once '../dgr/dogru_libs_net/LibLoader.php';
+$DebugIsActive = false;
+LibLoader::Load(LIB_PAGECONTROLLER);
+LibLoader::Load(LIB_DEBUG);
+LibLoader::Load(LIB_TEMA);
+Debug::$IsAktif = false;
+LibLoader::Load(LIB_MYSQL);
+
+$TEMA = 't-demo';
+$TEMA_ADRES_SATIRI_SECIMI = false;
+$SITE_ADI = 'Product PMS Server';
